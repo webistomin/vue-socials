01b1d8707db04fcc3ac08edcbaceb9a9781ee0aa
webistomin <webistomin@gmail.com>
2021-01-24 13:04:50 +0300

    feat(typings): add typings to all components

--- src/types/vue-socials.d.ts
+++
@@ -1,7 +0,0 @@
-import Vue, { PluginFunction, VueConstructor } from 'vue';
-
-declare const VueSocials: PluginFunction<never>;
-export default VueSocials;
-
-export const SVkontakte: VueConstructor<Vue>;
-export const SFacebook: VueConstructor<Vue>;
---
+++ src/vue-socials.d.ts
@@ -0,0 +1,292 @@
+import { PluginFunction } from 'vue';
+
+declare const VueSocials: PluginFunction<never>;
+
+/**
+ * All Components
+ *
+ * @note Dear contributors,
+ * Components listed in this file should be listed alphabetically. Thanks :)
+ */
+
+/**
+ * B
+ */
+import SBloggerComponent from './components/SBlogger';
+declare const SBlogger: typeof SBloggerComponent;
+
+/**
+ * D
+ */
+import SDevToComponent from './components/SDevTo';
+declare const SDevTo: typeof SDevToComponent;
+
+import SDiasporaComponent from './components/SDiaspora';
+declare const SDiaspora: typeof SDiasporaComponent;
+
+import SDoubanComponent from './components/SDouban';
+declare const SDouban: typeof SDoubanComponent;
+
+/**
+ * E
+ */
+import SEmailComponent from './components/SEmail';
+declare const SEmail: typeof SEmailComponent;
+
+import SEvernoteComponent from './components/SEvernote';
+declare const SEvernote: typeof SEvernoteComponent;
+
+/**
+ * F
+ */
+import SFacebookComponent from './components/SFacebook';
+declare const SFacebook: typeof SFacebookComponent;
+
+import SFacebookCountComponent from './components/SFacebookCount';
+declare const SFacebookCount: typeof SFacebookCountComponent;
+
+import SFacebookMessengerComponent from './components/SFacebookMessenger';
+declare const SFacebookMessenger: typeof SFacebookMessengerComponent;
+
+import SFacebookWorkplaceComponent from './components/SFacebookWorkplace';
+declare const SFacebookWorkplace: typeof SFacebookWorkplaceComponent;
+
+import SFlipBoardComponent from './components/SFlipBoard';
+declare const SFlipBoardWorkplace: typeof SFlipBoardComponent;
+
+/**
+ * G
+ */
+import SGithubComponent from './components/SGithub';
+declare const SGithub: typeof SGithubComponent;
+
+import SGithubCountComponent from './components/SGithubCount';
+declare const SGithubCount: typeof SGithubCountComponent;
+
+import SGithubGistComponent from './components/SGithubGist';
+declare const SGithubGist: typeof SGithubGistComponent;
+
+import SGithubRepoComponent from './components/SGithubRepo';
+declare const SGithubRepo: typeof SGithubRepoComponent;
+
+import SGithubRepoCountComponent from './components/SGithubRepoCount';
+declare const SGithubRepoCount: typeof SGithubRepoCountComponent;
+
+import SGmailComponent from './components/SGmail';
+declare const SGmail: typeof SGmailComponent;
+
+import SGoogleBookmarksComponent from './components/SGoogleBookmarks';
+declare const SGoogleBookmarks: typeof SGoogleBookmarksComponent;
+
+/**
+ * H
+ */
+import SHackerNewsComponent from './components/SHackerNews';
+declare const SHackerNews: typeof SHackerNewsComponent;
+
+import SHatenaComponent from './components/SHatena';
+declare const SHatena: typeof SHatenaComponent;
+
+import SHatenaCountComponent from './components/SHatenaCount';
+declare const SHatenaCount: typeof SHatenaCountComponent;
+
+import SHouzzComponent from './components/SHouzz';
+declare const SHouzzCount: typeof SHouzzComponent;
+
+/**
+ * I
+ */
+import SInstaPaperComponent from './components/SInstaPaper';
+declare const SInstaPaper: typeof SInstaPaperComponent;
+
+/**
+ * L
+ */
+import SLineComponent from './components/SLine';
+declare const SLine: typeof SLineComponent;
+
+import SLineCountComponent from './components/SLineCount';
+declare const SLineCount: typeof SLineCountComponent;
+
+import SLinkedInComponent from './components/SLinkedIn';
+declare const SLinkedIn: typeof SLinkedInComponent;
+
+import SLiveJournalComponent from './components/SLiveJournal';
+declare const SLiveJournal: typeof SLiveJournalComponent;
+
+/**
+ * M
+ */
+import SMailRuComponent from './components/SMailRu';
+declare const SMailRu: typeof SMailRuComponent;
+
+import SMicrosoftTeamsComponent from './components/SMicrosoftTeams';
+declare const SMicrosoftTeams: typeof SMicrosoftTeamsComponent;
+
+/**
+ * O
+ */
+import SOdnoklassnikiComponent from './components/SOdnoklassniki';
+declare const SOdnoklassniki: typeof SOdnoklassnikiComponent;
+
+import SOdnoklassnikiCountComponent from './components/SOdnoklassnikiCount';
+declare const SOdnoklassnikiCount: typeof SOdnoklassnikiCountComponent;
+
+/**
+ * P
+ */
+import SPinterestComponent from './components/SPinterest';
+declare const SPinterest: typeof SPinterestComponent;
+
+import SPinterestCountComponent from './components/SPinterestCount';
+declare const SPinterestCount: typeof SPinterestCountComponent;
+
+import SPocketComponent from './components/SPocket';
+declare const SPocket: typeof SPocketComponent;
+
+/**
+ * Q
+ */
+import SQZoneComponent from './components/SQZone';
+declare const SQZone: typeof SQZoneComponent;
+
+/**
+ * R
+ */
+import SRedditComponent from './components/SReddit';
+declare const SReddit: typeof SRedditComponent;
+
+import SRedditCountComponent from './components/SRedditCount';
+declare const SRedditCount: typeof SRedditCountComponent;
+
+import SRenRenComponent from './components/SRenRen';
+declare const SRenRen: typeof SRenRenComponent;
+
+/**
+ * S
+ */
+import SSkypeComponent from './components/SSkype';
+declare const SSkype: typeof SSkypeComponent;
+
+import SSmsComponent from './components/SSms';
+declare const SSms: typeof SSmsComponent;
+
+/**
+ * T
+ */
+import STelegramComponent from './components/STelegram';
+declare const STelegram: typeof STelegramComponent;
+
+import SThreemaComponent from './components/SThreema';
+declare const SThreema: typeof SThreemaComponent;
+
+import STrelloComponent from './components/STrello';
+declare const STrello: typeof STrelloComponent;
+
+import STumblrComponent from './components/STumblr';
+declare const STumblr: typeof STumblrComponent;
+
+import STumblrCountComponent from './components/STumblrCount';
+declare const STumblrCount: typeof STumblrCountComponent;
+
+import STwitterComponent from './components/STwitter';
+declare const STwitter: typeof STwitterComponent;
+
+/**
+ * V
+ */
+import SViberComponent from './components/SViber';
+declare const SViber: typeof SViberComponent;
+
+import SVkontakteComponent from './components/SVkontakte';
+declare const SVkontakte: typeof SVkontakteComponent;
+
+import SVkontakteCountComponent from './components/SVkontakteCount';
+declare const SVkontakteCount: typeof SVkontakteCountComponent;
+
+/**
+ * W
+ */
+import SWeiboComponent from './components/SWeibo';
+declare const SWeibo: typeof SWeiboComponent;
+
+import SWhatsAppComponent from './components/SWhatsApp';
+declare const SWhatsApp: typeof SWhatsAppComponent;
+
+import SWordPressComponent from './components/SWordPress';
+declare const SWordPress: typeof SWordPressComponent;
+
+/**
+ * X
+ */
+import SXingComponent from './components/SXing';
+declare const SXing: typeof SXingComponent;
+
+/**
+ * Y
+ */
+import SYahooComponent from './components/SYahoo';
+declare const SYahoo: typeof SYahooComponent;
+
+import SYammerComponent from './components/SYammer';
+declare const SYammer: typeof SYammerComponent;
+
+export {
+  SBlogger,
+  SDevTo,
+  SDiaspora,
+  SDouban,
+  SEmail,
+  SEvernote,
+  SFacebook,
+  SFacebookCount,
+  SFacebookMessenger,
+  SFacebookWorkplace,
+  SFlipBoardWorkplace,
+  SGithub,
+  SGithubCount,
+  SGithubGist,
+  SGithubRepo,
+  SGithubRepoCount,
+  SGmail,
+  SGoogleBookmarks,
+  SHackerNews,
+  SHatena,
+  SHatenaCount,
+  SHouzzCount,
+  SInstaPaper,
+  SLine,
+  SLineCount,
+  SLinkedIn,
+  SLiveJournal,
+  SMailRu,
+  SMicrosoftTeams,
+  SOdnoklassniki,
+  SOdnoklassnikiCount,
+  SPinterest,
+  SPinterestCount,
+  SPocket,
+  SQZone,
+  SReddit,
+  SRedditCount,
+  SRenRen,
+  SSkype,
+  SSms,
+  STelegram,
+  SThreema,
+  STrello,
+  STumblr,
+  STumblrCount,
+  STwitter,
+  SViber,
+  SVkontakte,
+  SVkontakteCount,
+  SWeibo,
+  SWhatsApp,
+  SWordPress,
+  SXing,
+  SYahoo,
+  SYammer,
+};
+
+export default VueSocials;
